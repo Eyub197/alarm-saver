6737dbb7078c2cb1f4908bb80434b31d0f8ceea0
Eyub197 <eyubmehmed122@gmail.com>
2026-04-13 22:28:08 +0300

    batman

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # alarm-saver
+
+# The problem :
+No free calendar offers alarms with sounds and notifications
+
+# Solutoin
+Create my custom alarm app cli for now
---
+++ index.ts
@@ -0,0 +1,5 @@
+console.log("Hello via Bun!");
+
+// get work blocks from google calendar api
+// set notifications with notify send
+// optoinal add gimini-flash-lite for cusotom alarm messages depending on the next event turn offable
